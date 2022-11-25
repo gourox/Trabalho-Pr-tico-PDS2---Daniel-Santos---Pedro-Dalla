65dad5e6bb7cca7d5ed56bfa00b9484584face13
Pedro Dalla-Lana <55250336+Cryomanc3r@users.noreply.github.com>
2022-11-25 05:26:58 -0300

    Update Normaliza_Palavra.h

--- Codigo Fonte/Normaliza_Palavra.h
+++ Codigo Fonte/Normaliza_Palavra.h
@@ -1,9 +1,19 @@
-#include <string.h>
+#ifndef NORMALIZA_PALAVRA_H
+#define NORMALIZA_PALAVRA_H
+
+#include <string>
+
+using std::string;
 
 class NormalizaPalavra {
     public:
 
-void RemoveCaracteresEspeciais();
+void RemoveCaracteresEspeciais(std::string _palavra);
 void ConverteEmMinusculas();
 void RetornaNormalizada();
+
+    private:
+    string* _palavra;
 };
+
+#endif
